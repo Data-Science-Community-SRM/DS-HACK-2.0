1d0f217e1b42a66c5e16ad8e159ae2311fd7d5b6
alvinbengeorge <alvinallen333@gmail.com>
2023-12-28 19:53:56 +0530

    fix: place evenly

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -17,7 +17,7 @@ const Sponsors = () => {
             </div>
             <div>
                 <h3 className="tier-title text-center">Gold tier</h3>
-                <div className="flex gap-16 justify-center">
+                <div className="flex gap-16 place-content-evenly">
                     <div className="w-36 h-36">
                         <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
@@ -40,7 +40,7 @@ const Sponsors = () => {
             </div>
             <div>
                 <h3 className="tier-title text-center">Silver tier</h3>
-                <div className="flex gap-16 justify-center">
+                <div className="flex gap-16 place-content-evenly">
                     <div className="w-36 h-36">
                         <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
