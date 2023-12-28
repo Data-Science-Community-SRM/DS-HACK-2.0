a71f16f6dea4444ce8e14d999fc1727fc7a77724
alvinbengeorge <alvinallen333@gmail.com>
2023-12-28 18:57:41 +0530

    feat: Sponsors page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
     <Prizes/>
     <Schedule/>
     <Cheif/>
+  
     <Sponsors/>
     <Footer/>
    </div>
--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -1,7 +1,69 @@
 const Sponsors = () => {
-    return ( 
-        <p>this is sponsors</p>
-     );
+    return (
+        <section id="sponsors" className="bg-scroll section-bg">
+            <div>
+                <h2 className="section-title text-4xl text-center p-8">Sponsors</h2>
+            </div>
+            <div>
+                <h3 className="tier-title text-center">Platinum tier</h3>
+                <div className="flex grid grid-col-1 justify-around">
+                    <div className="  w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 className="tier-title text-center">Gold tier</h3>
+                <div className="flex gap-16 justify-center">
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3 className="tier-title text-center">Silver tier</h3>
+                <div className="flex gap-16 justify-center">
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                    <div className="w-36 h-36">
+                        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 24.7742L26.8387 0H118.34L144 24.7742V121.032L118.34 144H26.8387L0 121.032V24.7742Z" fill="#660066" />
+                        </svg>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
 }
- 
+
 export default Sponsors;
