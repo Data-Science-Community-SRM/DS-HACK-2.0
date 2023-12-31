c269c4a4725d7d92fba47a191794d64a12bfa90c
Yogita Jha <yogitajha05@gmail.com>
2023-12-31 16:02:50 +0530

    about and tracks page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,24 @@
 const About = () => {
     return ( 
-        <p>this is about page</p>
-     );
-}
- 
+        <div className="relative flex flex-col items-center justify-center bg-fuchsia-800 p-5 text-white w-90vw h-screen overflow-hidden transition all">
+          <img src="../../../public/about/Group 3.png" alt="logo" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[60%] h-[95%] max-w-full max-h-full max-sm:h-[60%] max-sm:w-[73%] sm:w-[60%] max-md:h-[75%]"/>
+          <img src="../../../public/about/Component 13.png" alt="Component-13" className="absolute top-0 left-0 w-[15%] h-auto max-sm:w-[35%] max-sm:hidden" />
+          <img src="../../../public/about/Component 15.png" alt="Component-15" className="absolute bottom-0 right-0 w-[10%] h-auto max-sm:w-[25%] max-sm:hidden" />
+          <img src="../../../public/about/Component 19.png" alt="Component-19" className="absolute w-[24%] h-[38%] bottom-[2%] left-0 max-sm:w-[28%] max-sm:h-[25%] max-sm:bottom-[17%] max-sm:left-[5%]" />
+          <img src="../../../public/about/Component 20.png" alt="Component-20" className="absolute top-[5%] w-[24%] h-[38%] right-[4%] max-sm:w-[28%] max-sm:h-[25%] max-sm:top-[15%] max-sm:right-[2%]"/>
+          <img src="../../../public/about/Rectangle 16.png" alt="Rectangle-16" className="absolute top-[30%] left-[7%] w-[86%] h-[48%] max-sm:h-[40%] max-sm:w-[80%] max-sm:top-[30%] max-sm:left-[13%]" />
+          <img src="../../../public/about/Rectangle 60.png" alt="Rectangle-60" className="absolute top-[32%] left-[8%] w-[84%] h-[43.5%] max-sm:h-[38%] max-sm:w-[78%] max-sm:top-[31%] max-sm:left-[14%]" />
+          <img src="../../../public/about/colour.png" alt="colour" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[60%] h-[95%] max-w-full max-h-full max-w-full max-h-full max-sm:h-[60%] max-sm:w-[73%] max-md:w-[60%] max-md:h-[75%]" />
+          <img src="../../../public/about/Vector 59.png" alt="Vector-59" className="absolute bottom-[21%] right-[6.5%] w-[24%] h-[29%] max-sm:w-[15%] max-sm:bottom-[29%] max-sm:h-[24%] max-sm:right-[6%]" />
+          <img src="../../../public/about/Vector 60.png" alt="Vector-60" className="absolute top-[29%] left-[6.5%] w-[19%] h-[30%] max-sm:w-[15%] max-sm:top-[29%] max-sm:h-[25%] max-sm:left-[12%]" />
+          <img src="../../../public/about/Vector 61.png" alt="Vector-61" className="absolute bottom-[20%] right-[6%] w-[11%] h-[12%] max-sm:w-[8%] max-sm:bottom-[28%] max-sm:h-[8%] max-sm:right-[5%]" />
+          <img src="../../../public/about/Component 22.png" alt="Component-22" className="absolute bottom-[10%] right-[10%] w-[10%] h-[7%] max-sm:w-[16%] max-sm:bottom-[21%] max-sm:h-[5%] max-sm:right-[10%]" />
+          <img src="../../../public/about/Component 23.png" alt="Component-23" className="absolute top-[23%] left-[8%] w-[8%] h-[5%]max-sm:w-[18%] max-sm:top-[24%] max-sm:h-[3.5%] max-sm:left-[16%]" />
+          <img src="../../../public/about/Ellipse 28.png" alt="Ellipse-28" className="absolute top-[84%] left-[34%] w-[1.8%] h-[2.2%]" />
+          <img src="../../../public/about/Ellipse 29.png" alt="Ellipse-29" className="absolute top-[88%] left-[32%] w-[1.8%] h-[2.2%]" />
+          <img src="../../../public/about/Ellipse 27.png" alt="Ellipse-27" className="absolute top-[89.3%] left-[37%] w-[1.8%] h-[2.2%]" />
+          <p className="absolute flex items justify-center text-fuchsia-300 text-6xl left-[30%] right-[30%] top-[10%] font-bold max-sm:text-4xl max-sm:left-[34%] sm:text-5xl">ABOUT</p>
+      </div>
+    );
+  };
 export default About;
