0f8ca186087e022dc04c70ab6cf60b1d42dd0e7c
Anubhav Pathak <anubhavpathak99@gmail.com>
2024-01-05 23:40:54 +0530

    Added Logos in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,14 @@ export default function App() {
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="navbar absolute top-2 left-1/2 -translate-x-1/2 z-10 bg-secondary rounded-full max-w-3xl">
+        <div className="navbar absolute top-2 left-1/2 -translate-x-1/2 z-10 bg-secondary rounded-full max-w-3xl text-white">
           <div className="navbar-start">
             <div className="flex-none lg:hidden">
               <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
               </label>
             </div> 
-            <a href="/" className="btn btn-ghost text-xl">{/* Place DS Hack 2.0 Logo Here */}</a>
+            <a href="/" className="btn btn-ghost text-xl"><img src="DSHackLogo.png" alt="DSC Logo" className="w-12 h-12"/></a>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
@@ -38,7 +38,7 @@ export default function App() {
             </ul>
           </div>
           <div className="navbar-end hidden lg:flex">
-            {/* Place DSC Logo Here */}
+            <img src="/DSC.png" alt="DSC Logo" className="w-12 h-12 rounded-full"/>
           </div>
         </div>
         {/* Page content here */}
